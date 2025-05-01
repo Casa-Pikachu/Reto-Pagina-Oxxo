85e90c6c368c1975e82022392abe83909b1fd812
nina-col <giannina@colunga.net>
2025-04-30 20:07:16 -0600

    pantalla magda bien

---
+++ Pagina_Oxxo/Pagina_Oxxo/Pages/Podio.cstml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pagina_Oxxo.Pages;
+
+public class PodioModel : PageModel
+{
+   public void OnGet()
+    {
+
+    }
+}
