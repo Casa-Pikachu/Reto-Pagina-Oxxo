ec0c11e6b7c5a54a50cec71b0c96dea836d0299a
Bruno Zabala <brunozabalap@gmail.com>
2025-05-02 08:44:24 -0600

    Deleted unnecessary files + changed page titles

--- Pagina_Oxxo/Pagina_Oxxo/Model/DataBaseContext.cs
+++ Pagina_Oxxo/Pagina_Oxxo/Model/DataBaseContext.cs
@@ -230,91 +230,87 @@ namespace Pagina_Oxxo.Model{
                     */
                     
 
-        MySqlCommand cmd = new MySqlCommand(query, conexion);
-        
+            MySqlCommand cmd = new MySqlCommand(query, conexion);
+            
 
-        using (var reader = cmd.ExecuteReader())
-        {
-            while (reader.Read())
+            using (var reader = cmd.ExecuteReader())
             {
-                //nuevo objeto para agregar los valores 
-                Podios usrpod = new Podios
+                while (reader.Read())
                 {
-                    nombre = reader["nombre"].ToString(),
-                    apellido = reader["apellido"].ToString(),
-                    puntaje = Convert.ToInt32(reader["puntaje"])
-                    
-                };
+                    //nuevo objeto para agregar los valores 
+                    Podios usrpod = new Podios
+                    {
+                        nombre = reader["nombre"].ToString(),
+                        apellido = reader["apellido"].ToString(),
+                        puntaje = Convert.ToInt32(reader["puntaje"])
+                        
+                    };
 
-                PodioUsuarios.Add(usrpod);
-                //se le agrega
-            
+                    PodioUsuarios.Add(usrpod);
+                    //se le agrega
+                
+                }
+            }
+
+            conexion.Close();
+
+            return PodioUsuarios;
         }
-    }
-
-    conexion.Close();
-
-    return PodioUsuarios;
-}
 
 
-//Obtener el medallero (cada lugar individual para ponerlo en las tarjetas)
-public Podios GetLugarMedalla(int posicion){
+        //Obtener el medallero (cada lugar individual para ponerlo en las tarjetas)
+        public Podios GetLugarMedalla(int posicion){
 
-    //no es lista asi que pues nada mas se inicia como null
-            Podios resultado = null;
-            
-            MySqlConnection conexion = GetConnection();
-            conexion.Open();
-            //$ porque salia error 
-            string query = $@"
-                    SELECT nombre, apellido, puntaje
-        FROM (
-            SELECT 
-                u.nombre, 
-                u.apellido, 
-                r.puntaje,
-                ROW_NUMBER() OVER (ORDER BY r.puntaje DESC) AS posicion
-            FROM usuarios u
-            JOIN ranking r ON u.id_usuario = r.id_usuario
-        ) AS sub  
-        WHERE posicion = {posicion};";
-
-
-        /*se realiza una consulta sql, muy parecida a la anterior con unos cambios.
-        se seleccionan los parametros de las tablas usuarios y ranking, para esto es necesario
-        unirlas con JOIN por medio de lo que tienen su PK y FK, que es el id de usuario. 
-
-        para obtener la posicion se hizo un row_number que saca el numero de fila una vez 
-        que se ordena de mayor a menor. Todo esto queda dentro de una subconsulta 
-        en la que luego se puede hacer la consulta se pone la variable que se desea. Por ejemplo 
-        ganador se usa la posicion 1.  
-        */
-
-        MySqlCommand cmd = new MySqlCommand(query, conexion);
-
-        using (var reader = cmd.ExecuteReader())
-        {
-            if (reader.Read())
-
-            //objeto para agregar valores
-            resultado = new Podios
-                {
-                    nombre = reader["nombre"].ToString(),
-                    apellido = reader["apellido"].ToString(),
-                    puntaje = Convert.ToInt32(reader["puntaje"])
+            //no es lista asi que pues nada mas se inicia como null
+                    Podios resultado = null;
                     
-                };
-    }
+                    MySqlConnection conexion = GetConnection();
+                    conexion.Open();
+                    //$ porque salia error 
+                    string query = $@"
+                            SELECT nombre, apellido, puntaje
+                FROM (
+                    SELECT 
+                        u.nombre, 
+                        u.apellido, 
+                        r.puntaje,
+                        ROW_NUMBER() OVER (ORDER BY r.puntaje DESC) AS posicion
+                    FROM usuarios u
+                    JOIN ranking r ON u.id_usuario = r.id_usuario
+                ) AS sub  
+                WHERE posicion = {posicion};";
 
-    conexion.Close();
 
-    return resultado; 
+                /*se realiza una consulta sql, muy parecida a la anterior con unos cambios.
+                se seleccionan los parametros de las tablas usuarios y ranking, para esto es necesario
+                unirlas con JOIN por medio de lo que tienen su PK y FK, que es el id de usuario. 
 
+                para obtener la posicion se hizo un row_number que saca el numero de fila una vez 
+                que se ordena de mayor a menor. Todo esto queda dentro de una subconsulta 
+                en la que luego se puede hacer la consulta se pone la variable que se desea. Por ejemplo 
+                ganador se usa la posicion 1.  
+                */
 
-    }
-}
-}
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+
+                    //objeto para agregar valores
+                    resultado = new Podios
+                        {
+                            nombre = reader["nombre"].ToString(),
+                            apellido = reader["apellido"].ToString(),
+                            puntaje = Convert.ToInt32(reader["puntaje"])
+                            
+                        };
+            }
+
+            conexion.Close();
+
+            return resultado; 
+        }
 
         public List<Ranking> GetRanking(){
             List<Ranking> ListaRank = new List<Ranking>();
