438f3d099953f4633df5e44e0ce83d7772247851
Bruno Zabala <brunozabalap@gmail.com>
2025-05-01 23:07:59 -0600

    Base de datos + backend Inicio

--- Pagina_Oxxo/Pagina_Oxxo/Pages/Index.cshtml.cs
+++ Pagina_Oxxo/Pagina_Oxxo/Pages/Index.cshtml.cs
@@ -1,19 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pagina_Oxxo.Model;
 
 namespace Pagina_Oxxo.Pages;
 
-public class IndexModel : PageModel
-{
-    private readonly ILogger<IndexModel> _logger;
+public class IndexModel : PageModel{
+    private readonly DataBaseContext _context;
+    public Usuarios user;
+    public List<Anuncios> anuncios = new List<Anuncios>();
+    public List<Usuarios> top3 = new List<Usuarios>();
+    public List<Recompensas> recompensas = new List<Recompensas>();
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
+    public IndexModel(DataBaseContext context){
+        _context = context;
     }
 
-    public void OnGet()
-    {
-
+    public void OnGet(){
+        user = _context.GetUserInfo("Bruno", "Zabala");
+        anuncios = _context.GetAnuncios("Bruno", "Zabala");
+        top3 = _context.GetTop3();
+        recompensas = _context.GetRecompensas();
     }
 }
