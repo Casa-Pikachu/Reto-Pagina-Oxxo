7fc8dcd39491569bf92cbc3d900c1fbcdfc415a6
Angel <a00838418@tec.mx>
2025-06-10 13:07:56 -0600

    Completed Dashboard Angel
    Complete the dashboard and connect it to the asesor's view

--- Pagina_Oxxo/Pagina_Oxxo/Pages/Dashboard.cshtml.cs
+++ Pagina_Oxxo/Pagina_Oxxo/Pages/Dashboard.cshtml.cs
@@ -1,13 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Pagina_Oxxo.Model;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Net;
 
 namespace Pagina_Oxxo.Pages;
 
 public class DashboardModel : PageModel
 {
 
+    private readonly DataBaseContext _context;
+    public int id_usuario;
+
+    public DashboardModel(DataBaseContext context)
+    {
+        _context = context;
+    }
+
+    public void OnGet()
+    {
+        int id = 0;
+        if (HttpContext.Session.GetInt32("id_usuario") is int sessionId)
+        {
+            id_usuario = sessionId;
+            id = sessionId;
+        }
+        else
+        {
+            id_usuario = 0;
+        }
+
+        if (id != 0)
+        {
+            var jsonParams = $"{{\"id_usuario_param\":{id}}}";
+            var encodedParams = WebUtility.UrlEncode(jsonParams);
+
+            var lookerUrl = $"https://lookerstudio.google.com/embed/u/0/reporting/f02c7f70-da8f-4949-9f28-806a6cd6ba45/page/8ESNF?params={encodedParams}";
+
+            ViewData["lookerUrl"] = lookerUrl;
+        }
+        else
+        {
+            ViewData["lookerUrl"] = "https://lookerstudio.google.com/embed/u/0/reporting/f02c7f70-da8f-4949-9f28-806a6cd6ba45/page/8ESNF";
+        }
+
+    }
 
 
-    
 }
